7a16e4f2c061cc9eb340ad22adda587764a3cd65
Robert Neville <robertneville27@gmail.com>
2022-11-05 13:07:59 +1000

    add test to verify navigation to login page

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-describe('app layout', () => {
+describe('App', () => {
 
   test('renders header with logo and theme button', () => {
     render(<App />);
@@ -15,9 +16,33 @@ describe('app layout', () => {
 
   test('renders landing page as initial route', () => {
     render(<App />);
-    const pageHeading = screen.getByText(/Welcome!/i);
+    const pageHeading = screen.getByText(/Welcome!/);
     expect(pageHeading).toBeInTheDocument();
-    const pageParagraph = screen.getByText(/Projectable is a lightweight/i);
+    const pageParagraph = screen.getByText(/Projectable is a lightweight/);
     expect(pageParagraph).toBeInTheDocument();
+  });
+
+  test('renders login page when login link is clicked', () => {
+    render(<App />);
+    const loginLink = screen.getByText(/Login/);
+    userEvent.click(loginLink);
+
+    const pageHeading = screen.getByText(/Login/);
+    expect(pageHeading).toBeInTheDocument();
+  
+    const form = document.querySelector('form');
+    expect(form).toBeInTheDocument();
+
+    const labels = form.querySelectorAll('label');
+    expect(labels.length).toBe(2);
+    
+    const inputs = form.querySelectorAll('input');
+    expect(inputs.length).toBe(2);
+    
+    const buttons = form.querySelectorAll('button');
+    expect(buttons.length).toBe(2);
+
+    const p = screen.getByText(/Don't have an account\?/);
+    expect(p).toBeInTheDocument();
   });
 });

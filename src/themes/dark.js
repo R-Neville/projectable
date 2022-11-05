1119fedbfb290295d2cf9c09cf05ee4154306d1b
Robert Neville <robertneville27@gmail.com>
2022-11-05 12:40:21 +1000

    refactor dark theme color order

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,8 +1,8 @@
 const dark = {
-  bgAccent: '#373E48',
-  fgAccent: '#DBDBE3',
   bgPrimary: '#535D6C',
   fgPrimary: '#DBDBE3',
+  bgAccent: '#373E48',
+  fgAccent: '#DBDBE3',
   bgHighlight: '#6F7C91',
   fgHighlight: '#DBDBE3',
 };

c00017339f777cd79d0a0800587ed11dc5b600e5
Robert Neville <robertneville27@gmail.com>
2022-11-05 14:08:39 +1000

    add test for verifying the effectiveness of clear button in register page content

---
+++ src/components/RegisterContent.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+beforeAll(() => {
+  render(<App />);
+  const registerLink = screen.getByText(/Register/);
+  userEvent.click(registerLink);
+});
+
+describe('register page content', () => {
+  describe('clear action', () => {
+    test('clears register form', () => {
+      const inputs = document.querySelectorAll('input');
+      inputs.forEach((input) => {
+        input.value = 'Test';
+      });
+      const clearButton = screen.getByText(/Clear/);
+      userEvent.click(clearButton);
+      inputs.forEach((input) => {
+        expect(input.value).toBe('');
+      });
+    });
+  });
+});

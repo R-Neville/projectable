62a6d35b0ba02a83794f8a973b45147009c6f30e
Robert Neville <robertneville27@gmail.com>
2022-11-06 10:58:26 +1000

    create tests for Label component

---
+++ src/components/Label.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import ThemeProvider from '../ThemeProvider';
+import themes from '../themes';
+import Label from './Label';
+
+beforeEach(() => {
+  render(
+    <ThemeProvider>
+      <Label text={'test'} />
+    </ThemeProvider>
+  );
+});
+
+describe('Label', () => {
+  test('renders successfully', () => {
+    const label = screen.getByText(/test/);
+    expect(label).toBeInTheDocument();
+  });
+
+  test('has correct color', () => {
+    const label = screen.getByText(/test/);
+    // Default theme is light
+    expect(label).toHaveStyle({ color: themes.light.fgPrimary });
+  });
+});

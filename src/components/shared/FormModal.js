b330901aa74853772cf2c43de06d93eda8e7298a
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-10 08:40:19 +1100

    added styles to FormModal component

--- src/components/shared/FormModal.js
+++ src/components/shared/FormModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
+import Section from './Section';
 import Fieldset from './Fieldset';
+import FormActions from './FormActions';
 import Label from './Label';
 import Input from './Input';
 import TextArea from './TextArea';
@@ -12,33 +14,39 @@ export default function FormModal({ open, onClose }) {
     <>
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <section className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <form
-              action=""
-              className="register-form  sm:my-8 sm:w-full sm:max-w-lg"
-            >
-              <Fieldset
-                content={
-                  <>
-                    <Label text=" Project Name" />
-                    <Input type="text" name="name" />
-                  </>
-                }
-              />
-              <Fieldset content={<>
-              <Label text="Project Description"/>
-              <TextArea type="text" name="description"/>
-              </>}>
-               
-              </Fieldset>
-              <button
-                className="inline-flex rounded-md border border-transparent px-4 py-2 text-base font-medium"
-                onClick={onClose}
-              >
-                submit
-              </button>
-            </form>
-          </section>
+          <div className="flex min-h-full sm:items-center sm:p-0">
+            <Section
+              content={
+                <form
+                  action=""
+                  className="register-form  sm:my-8 sm:w-full sm:max-w-lg"
+                >
+                  <Fieldset
+                    content={
+                      <>
+                        <Label text=" Project Name" />
+                        <Input type="text" name="name" />
+                      </>
+                    }
+                  />
+                  <Fieldset
+                    content={
+                      <>
+                        <Label text="Project Description" />
+                        <TextArea type="text" name="description" />
+                      </>
+                    }
+                  ></Fieldset>
+                  <FormActions
+                    actions={[
+                      { text: 'Cancel', onClick: onClose,  type: 'reset' },
+                      { text: 'Submit',  type: 'submit' },
+                    ]}
+                  />
+                </form>
+              }
+            />
+          </div>
         </div>
       </div>
     </>,

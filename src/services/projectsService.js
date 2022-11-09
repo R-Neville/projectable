a442825a4d19c8f78e97ac46749d9657b5f01336
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-09 13:36:01 +1100

    added api service functions for project calls

---
+++ src/services/projectsService.js
@@ -0,0 +1,41 @@
+import projectableAPI from '../config/axiosConfig';
+
+export const getAllProjects = async () => {
+  const data = await projectableAPI.get('/api/projects');
+  return data;
+};
+
+export const getProject = async (projectId) => {
+  const data = await projectableAPI.get(`/api/projects/${projectId}`);
+  return data;
+};
+
+export const createProject = async (formData) => {
+  const data = await projectableAPI.post('/api/projects', formData);
+  return data;
+};
+
+export const updateProject = async (projectId, formData) => {
+  const data = await projectableAPI.put(`/api/projects/${projectId}`, formData);
+  return data;
+};
+
+export const addMember = async (projectId, member) => {
+  const data = await projectableAPI.put(
+    `/api/projects/${projectId}/members/new`,
+    member
+  );
+  return data;
+};
+
+export const searchForUsers = async (query) => {
+  const data = await projectableAPI.get(
+    `/api/projects/members/search/email?=${query}`
+  );
+  return data;
+};
+
+export const deleteProject = async (projectId) => {
+  const data = await projectableAPI.delete(`/api/projects/${projectId}`);
+  return data;
+};

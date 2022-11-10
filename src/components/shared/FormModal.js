2ff8f5a539c0e98604827477cf6b818806565fc1
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-10 11:22:02 +1100

    fixed typo

--- src/components/shared/FormModal.js
+++ src/components/shared/FormModal.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createProject } from '../../services/projectsService';
 import { createPortal } from 'react-dom';
 import Section from './Section';
 import Fieldset from './Fieldset';
@@ -6,8 +9,40 @@ import FormActions from './FormActions';
 import Label from './Label';
 import Input from './Input';
 import TextArea from './TextArea';
+import FormError from './FormError';
 
 export default function FormModal({ open, onClose }) {
+  const initialFormState = {
+    name: '',
+    description: '',
+  };
+
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState(initialFormState);
+  const [formError, setFormError] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value)
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formState.name.length === 0 || formState.description.length === 0) {
+      setFormError('Required fields are empty');
+    } else {
+      createProject({ ...formState })
+        .then((project) => {
+          // set state here, Context maybe?
+          console.log(project)
+          navigate('/projects');
+        })
+        .catch((error) => {
+          setFormError(error.message);
+        });
+    }
+  };
+
   if (!open) return null;
 
   return createPortal(
@@ -17,15 +52,22 @@ export default function FormModal({ open, onClose }) {
           <div className="flex min-h-full sm:items-center sm:p-0">
             <Section
               content={
-                <form
-                  action=""
-                  className="register-form  sm:my-8 sm:w-full sm:max-w-lg"
-                >
+                <form action="" className="register-form  sm:my-8 sm:w-full sm:max-w-lg">
+                  {formError && (
+                    <FormError
+                      text={formError}
+                      onDismiss={() => setFormError('')}
+                    />
+                  )}
                   <Fieldset
                     content={
                       <>
                         <Label text=" Project Name" />
-                        <Input type="text" name="name" />
+                        <Input
+                          type="text"
+                          name="name"
+                          onChange={onInputChange}
+                        />
                       </>
                     }
                   />
@@ -33,14 +75,18 @@ export default function FormModal({ open, onClose }) {
                     content={
                       <>
                         <Label text="Project Description" />
-                        <TextArea type="text" name="description" />
+                        <TextArea
+                          type="text"
+                          name="description"
+                          onChange={onInputChange}
+                        />
                       </>
                     }
-                  ></Fieldset>
+                 />
                   <FormActions
                     actions={[
-                      { text: 'Cancel', onClick: onClose,  type: 'reset' },
-                      { text: 'Submit',  type: 'submit' },
+                      { text: 'Cancel', onClick: onClose, type: 'reset' },
+                      { text: 'Submit', onClick: handleSubmit, type: 'submit' },
                     ]}
                   />
                 </form>
--- src/components/shared/TextArea.js
+++ src/components/shared/TextArea.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { useThemeContext } from '../../context-providers/ThemeProvider';
 
 
-export default function TextArea({type, name, onchange}) {
+export default function TextArea({type, name, onChange}) {
     const { theme } = useThemeContext();
 
   return (
     <textarea
       className=" flex flex-col w-full p-1 border-0 rounded outline-0  bg-white  mt-2"
       style={{ backgroundColor: theme.bgHighlight, color: theme.fgHighlight }}
-    ></textarea>
+      onChange={onChange}
+      onInput={onChange}
+      name={name}
+      type={type}
+    />
   );
 }

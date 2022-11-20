0ce87b297b7b2f691804ce9b4b3bddac7ebe4e8a
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-20 11:09:28 +1100

    added priority status to task card

--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -157,6 +157,9 @@ function DashboardContent({ dryRun }) {
                   title={task.brief}
                   content={
                     <div className="flex justify-between">
+                      <span style={{ color: theme.fgPrimary }}>
+                        {task.priority && `priority: ${task.priority}`}
+                      </span>
                       <span
                         style={{ color: theme.fgPrimary }}
                       >{`@${task.createdBy}`}</span>
